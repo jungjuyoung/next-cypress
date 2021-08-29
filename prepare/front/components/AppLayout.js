4db0766118d1b947df2e156d3f3a1492a35cae1e
jungjuyoung <nwwn1004@naver.com>
2021-08-30 04:43:13 +0900

    feat: antd setting, _app.js common file seperation, cypress setting

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'next/Link'
+import { Menu } from 'antd';
+
 const AppLayout = ({children}) => {
   return (
     <div>
-		<div>
-		<Link href="/"><a>노드버드</a></Link>
-		<Link href="/profile"><a>프로필</a></Link>
-		<Link href="/signup"><a>회원가입</a></Link>
-		</div>
+			<Menu mode="horizontal">
+				<Menu.Item key="nodebird">
+					<Link href="/"><a>노드버드</a></Link>
+				</Menu.Item>
+				<Menu.Item key="profile">
+					<Link href="/profile"><a>프로필</a></Link>
+				</Menu.Item>
+				<Menu.Item key="signup">
+					<Link href="/signup"><a>회원가입</a></Link>
+				</Menu.Item>
+		</Menu>
 		{children}
     </div>
   )
