458cacf780e1a41fd5fdeffdf0705489f003eaa5
jungjuyoung <nwwn1004@naver.com>
2021-08-30 23:55:48 +0900

    feat: added search bar and responsive layout

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'next/Link'
-import { Menu } from 'antd';
+import { Menu, Input, Row, Col } from 'antd';
 
+const {Search} = Input
 const AppLayout = ({children}) => {
   return (
     <div>
@@ -13,11 +14,31 @@ const AppLayout = ({children}) => {
 				<Menu.Item key="profile">
 					<Link href="/profile"><a>프로필</a></Link>
 				</Menu.Item>
+				<Menu.Item>
+				 <Search placeholder="search title"
+				  enterButton="Search"
+					style={{verticalAlign:'middle'}}
+				 />
+				</Menu.Item>
 				<Menu.Item key="signup">
 					<Link href="/signup"><a>회원가입</a></Link>
 				</Menu.Item>
 		</Menu>
-		{children}
+		<Row gutter={8}>
+		  <Col xs={24} md={6}>
+			  왼쪽메뉴
+			</Col>
+		  <Col xs={24} md={12}>
+			  {children}
+			</Col>
+		  <Col xs={24} md={6}>
+			  <a href="https://github.com/jungjuyoung" target="_blank"
+				  rel="noreferrer noopener"
+				>
+				  Nadia Github
+				</a>
+			</Col>
+		</Row>
     </div>
   )
 }
