db7e39dcb0f6ad6261647e4508c864a4c348846f
jungjuyoung <nwwn1004@naver.com>
2021-09-01 00:23:06 +0900

    feat: profile page and component on thepage complement

--- prepare/front/pages/profile.js
+++ prepare/front/pages/profile.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import Head from 'next/head'
+
 import AppLayout from '../components/AppLayout'
+import NicknameEditForm from '../components/NicknameEditForm'
+import FollowList from '../components/FollowList'
 
 const Profile = () => {
+  const followList = [{nickname: '제로초'},{nickname:'삥'},{nickname:'홀리'}]
+  const followingList = [{nickname: '제로초'},{nickname:'빠삐용'},{nickname:'오피셜'}]
   return (
     <>
       <Head>
@@ -10,7 +15,11 @@ const Profile = () => {
         <title>내프로필 | nodebird</title>
       </Head>
 
-    <AppLayout>프로필 페이지</AppLayout>
+    <AppLayout>
+    <NicknameEditForm/>
+    <FollowList header="팔로워 리스트" data={followList}/>
+    <FollowList header="팔로잉 리스트" data={followingList}/>
+    </AppLayout>
     </>
   )
 }
