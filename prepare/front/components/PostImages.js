0f270b2655223bdfd16d2cd7397907cac22cbcb3
jungjuyoung <nwwn1004@naver.com>
2021-09-09 04:39:56 +0900

    feat: create postimages component

--- prepare/front/components/PostImages.js
+++ prepare/front/components/PostImages.js
@@ -1,12 +1,44 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import PropTypes from 'prop-types'
+import { PlusOutlined } from '@ant-design/icons';
 
 const PostImages = ({images}) => {
-  return (
+	const [showImagesZoom, setShowImagesZoom] = useState(false)
+	const onZoom = useCallback(() => {
+    setShowImagesZoom(true)
+	}, [])
+
+  if(images.length === 1) {
+    return (
+      <div>
+        <img role="presentation" style={{maxHeight: 300, width: 'auto'}} src={images[0].src} alt={images[0].src} onClick={onZoom}/>
+      </div>
+    )
+  }
+	if(images.length === 2) {
+    return (
+      <div>
+        <img role="presentation" style={{width: '50%'}} src={images[0].src} alt={images[0].src} onClick={onZoom} />
+        <img role="presentation"  style={{width: '50%'}} src={images[1].src} alt={images[1].src} onClick={onZoom} />
+      </div>
+    )
+  }
+	return (
     <div>
-      구현중...
+      <img role="presentation" style={{width: '50%'}} src={images[0].src} alt={images[0].src} onClick={onZoom} />
+			<div
+          role="presentation"
+          style={{ display: 'inline-block', width: '50%', textAlign: 'center', verticalAlign: 'middle' }}
+          onClick={onZoom}
+        >
+			  <PlusOutlined/>
+				<br/>
+				{images.length - 1}
+				개의 사진 더보기
+			</div>
     </div>
   )
+  
 }
 
 PostImages.propTypes ={
