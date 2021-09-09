c03761ed7e27d306524d35c84e4fd2498d905111
jungjuyoung <nwwn1004@naver.com>
2021-09-10 00:38:43 +0900

    feat: postImage fixed

--- prepare/front/reducers/post.js
+++ prepare/front/reducers/post.js
@@ -8,10 +8,6 @@ export const initialState = {
     content: '첫 번째 게시글',
     Images: [{
       src: 'https://bookthumb-phinf.pstatic.net/cover/137/995/13799585.jpg?udate=20180726',
-    }, {
-      src: 'https://gimg.gilbut.co.kr/book/BN001958/rn_view_BN001958.jpg',
-    }, {
-      src: 'https://gimg.gilbut.co.kr/book/BN001998/rn_view_BN001998.jpg',
     }],
     Comments: [{
       User: {
