6a10d7657418b0dd1aeaab0be17b215a06fe2653
jungjuyoung <nwwn1004@naver.com>
2021-09-13 00:37:02 +0900

    feat:implement hashtag link

--- prepare/front/components/PostCard.js
+++ prepare/front/components/PostCard.js
@@ -1,71 +1,89 @@
-import React, { useState, useCallback } from 'react'
-import { useSelector } from 'react-redux'
-import PropTypes from 'prop-types'
+import React, { useState, useCallback } from "react";
+import { useSelector } from "react-redux";
+import PropTypes from "prop-types";
 
-import { Card, Button, Avatar, Form, Input, List, Comment, Popover } from 'antd';
-import { RetweetOutlined, HeartTwoTone, HeartOutlined, MessageOutlined, EllipsisOutlined } from '@ant-design/icons';
+import {
+  Card,
+  Button,
+  Avatar,
+  Form,
+  Input,
+  List,
+  Comment,
+  Popover,
+} from "antd";
+import {
+  RetweetOutlined,
+  HeartTwoTone,
+  HeartOutlined,
+  MessageOutlined,
+  EllipsisOutlined,
+} from "@ant-design/icons";
 
-import PostImages from './PostImages'
-import CommentForm from './CommentForm'
+import PostImages from "./PostImages";
+import CommentForm from "./CommentForm";
+import PostCardContent from "./PostCardContent";
 
 const PostCard = ({ post }) => {
-	const id = useSelector(state => state.user.me?.id)
-	const [liked, setLiked] = useState(false)
-	const [commentFormOpended, setCommentFormOpended] = useState(false)
-  const onToggleLike = useCallback(
-		() => {
-			setLiked(prev => !prev)
-		},[]
-	)
-  const onToggleCommentOpened = useCallback(
-		() => {
-			setCommentFormOpended(prev => !prev)
-		},[]
-	)
+  const id = useSelector(state => state.user.me?.id);
+  const [liked, setLiked] = useState(false);
+  const [commentFormOpended, setCommentFormOpended] = useState(false);
+  const onToggleLike = useCallback(() => {
+    setLiked(prev => !prev);
+  }, []);
+  const onToggleCommentOpened = useCallback(() => {
+    setCommentFormOpended(prev => !prev);
+  }, []);
 
   return (
-    <div style={{marginBottom:20}}>
-		  <Card
+    <div style={{ marginBottom: 20 }}>
+      <Card
         cover={post.Images[0] && <PostImages images={post.Images} />}
         actions={[
           <RetweetOutlined key="retweet" />,
-          liked
-            ? <HeartTwoTone twoToneColor="#eb2f96" key="heart" onClick={onToggleLike}/>
-            : <HeartOutlined key="heart"  onClick={onToggleLike}/>,
-          <MessageOutlined key="message" onClick={onToggleCommentOpened}/>,
+          liked ? (
+            <HeartTwoTone
+              twoToneColor="#eb2f96"
+              key="heart"
+              onClick={onToggleLike}
+            />
+          ) : (
+            <HeartOutlined key="heart" onClick={onToggleLike} />
+          ),
+          <MessageOutlined key="message" onClick={onToggleCommentOpened} />,
           <Popover
             key="ellipsis"
-            content={(
+            content={
               <Button.Group>
-                {id && post.UserId === id
-                  ? (
-                    <>
-                      <Button>수정</Button>
-                      <Button type="danger">삭제</Button>
-                    </>
-                  )
-                  : <Button>신고</Button>}
+                {id && post.UserId === id ? (
+                  <>
+                    <Button>수정</Button>
+                    <Button type="danger">삭제</Button>
+                  </>
+                ) : (
+                  <Button>신고</Button>
+                )}
               </Button.Group>
-            )}
+            }
           >
             <EllipsisOutlined />
           </Popover>,
         ]}
       >
-			<Card.Meta
-			  avatar={<Avatar>{post.User.nickName[0]}</Avatar>}
-			  title={post.User.nickName}
-			  description = {post.content}
-			/>
+        <Card.Meta
+          avatar={<Avatar>{post.User.nickName[0]}</Avatar>}
+          title={post.User.nickName}
+          description={<PostCardContent postData={post.content} />}
+        />
       </Card>
-			{commentFormOpended && (
-				<div>
-          <CommentForm post={post}/>
+      {commentFormOpended && (
+        <div>
+          <CommentForm post={post} />
           <List
             header={`${post.Comments.length} 댓글`}
             itemLayout="horizontal"
             dataSource={post.Comments}
-            renderItem={(item) => (
+            renderItem={item => (
               <li>
                 <Comment
                   author={item.User.nickName}
@@ -76,17 +94,17 @@ const PostCard = ({ post }) => {
             )}
           />
         </div>
-			)}
-		</div>
-  )
-}
+      )}
+    </div>
+  );
+};
 
 PostCard.propTypes = {
-	post: PropTypes.shape({
+  post: PropTypes.shape({
     User: PropTypes.object,
     content: PropTypes.string,
     img: PropTypes.string,
     createdAt: PropTypes.object,
   }),
-}
-export default PostCard
+};
+export default PostCard;
--- prepare/front/reducers/post.js
+++ prepare/front/reducers/post.js
@@ -1,49 +1,58 @@
 export const initialState = {
-  mainPosts: [{
-    id: 1,
-    User: {
+  mainPosts: [
+    {
       id: 1,
-      nickName: '찌미',
+      User: {
+        id: 1,
+        nickName: "찌미",
+      },
+      content: "오늘은 보람찬 하루였다. #오늘하루 #잘살았다.",
+      Images: [
+        {
+          src: "https://bookthumb-phinf.pstatic.net/cover/137/995/13799585.jpg?udate=20180726",
+        },
+        {
+          src: "https://gimg.gilbut.co.kr/book/BN001958/rn_view_BN001958.jpg",
+        },
+        {
+          src: "https://gimg.gilbut.co.kr/book/BN001998/rn_view_BN001998.jpg",
+        },
+      ],
+      Comments: [
+        {
+          User: {
+            nickName: "쿠키",
+          },
+          content: "이거 계속 공부하랬떠여",
+        },
+        {
+          User: {
+            nickName: "모니",
+          },
+          content: "얼른 사고싶어요~",
+        },
+      ],
     },
-    content: '첫 번째 게시글',
-    Images: [{
-      src: 'https://bookthumb-phinf.pstatic.net/cover/137/995/13799585.jpg?udate=20180726',
-    }, {
-      src: 'https://gimg.gilbut.co.kr/book/BN001958/rn_view_BN001958.jpg',
-    }, {
-      src: 'https://gimg.gilbut.co.kr/book/BN001998/rn_view_BN001998.jpg',
-    }],
-    Comments: [{
-      User: {
-        nickName: '쿠키',
-      },
-      content: '이거 계속 공부하랬떠여',
-    }, {
-      User: {
-        nickName: '모니',
-      },
-      content: '얼른 사고싶어요~',
-    }]
-  }],
+  ],
   imagePaths: [],
   postAdded: false,
 };
 
-const ADD_POST = 'ADD_POST'
+const ADD_POST = "ADD_POST";
 export const addPost = {
-  type: ADD_POST
-}
+  type: ADD_POST,
+};
 
 const dummyPost = {
   id: 2,
-  content: '더미데이터 입니다.',
+  content: "더미데이터 입니다.",
   User: {
     id: 1,
-    nickName: '징징이짱'
+    nickName: "징징이짱",
   },
   Images: [],
   Comments: [],
-}
+};
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
@@ -51,12 +60,11 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,
         mainPosts: [dummyPost, ...state.mainPosts],
-        postAdded: true
-      }
+        postAdded: true,
+      };
     default:
-      return state
+      return state;
       break;
   }
-
-}
-export default reducer
+};
+export default reducer;
