a2d4185f3a4cc062c13893adc1eb5c79267d3dbd
jungjuyoung <nwwn1004@naver.com>
2021-09-12 23:49:48 +0900

    fix: antdesign a unique problem solved;

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -7,19 +7,33 @@ import { Menu, Input, Row, Col } from 'antd';
 import UserProfile from './UserProfile'
 import LoginForm from './LoginForm'
 
-import styled from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
 
 const { Search } = Input
+
+const Global = createGlobalStyle`
+	.ant-row{
+		margin-right: 0 !important;
+		margin-left: 0 !important;
+	}
+	.ant-col:first-child{
+		padding-left: 0 !important;
+	}
+	.ant-col:last-child{
+		padding-right: 0 !important;
+	}
+`
 
 const SearchWrapper = styled(Search)`
  vertical-align:middle;
 `
 
-const AppLayout = ({children}) => {
+const AppLayout = ({ children }) => {
 	const { isLogin } = useSelector(state => state.user);
 
-  return (
-    <div>
+	return (
+		<div>
+			<Global />
 			<Menu mode="horizontal">
 				<Menu.Item key="nodebird">
 					<Link href="/"><a>노드버드</a></Link>
@@ -28,34 +42,34 @@ const AppLayout = ({children}) => {
 					<Link href="/profile"><a>프로필</a></Link>
 				</Menu.Item>
 				<Menu.Item>
-				 <SearchWrapper placeholder="search title"
-				  enterButton="Search"
-				 />
+					<SearchWrapper placeholder="search title"
+						enterButton="Search"
+					/>
 				</Menu.Item>
 				<Menu.Item key="signup">
 					<Link href="/signup"><a>회원가입</a></Link>
 				</Menu.Item>
-		 </Menu>
-		 <Row gutter={8}>
-		    <Col xs={24} md={6}>
-			    {isLogin? <UserProfile/> : <LoginForm/>}
-			  </Col>
-		    <Col xs={24} md={12}>
-			    {children}
-			  </Col>
-		    <Col xs={24} md={6}>
-			    <a href="https://github.com/jungjuyoung" target="_blank"
-				     rel="noreferrer noopener"
-				   >
-				   Nadia Github
-				  </a>
-			</Col>
-		 </Row>
-    </div>
-  )
+			</Menu>
+			<Row gutter={8}>
+				<Col xs={24} md={6}>
+					{isLogin ? <UserProfile /> : <LoginForm />}
+				</Col>
+				<Col xs={24} md={12}>
+					{children}
+				</Col>
+				<Col xs={24} md={6}>
+					<a href="https://github.com/jungjuyoung" target="_blank"
+						rel="noreferrer noopener"
+					>
+						Nadia Github
+					</a>
+				</Col>
+			</Row>
+		</div>
+	)
 }
 AppLayout.propTypes = {
-  children: PropTypes.node.isRequired,
+	children: PropTypes.node.isRequired,
 };
 
 export default AppLayout
