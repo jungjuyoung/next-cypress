ba8819c81e2db2ecafba18ef2c876696b35cf58e
jungjuyoung <nwwn1004@naver.com>
2021-09-19 00:37:55 +0900

    fix: redirection to the main page when directly entering the profile page without loggin in.

--- prepare/front/pages/profile.js
+++ prepare/front/pages/profile.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import Head from "next/head";
 
 import AppLayout from "../components/AppLayout";
 import NickNameEditForm from "../components/nicknameEditForm";
 import FollowList from "../components/FollowList";
+import Router from "next/router";
 
 const Profile = () => {
   const { me } = useSelector(state => state.user);
+  console.log(`me: ${JSON.stringify(me)}`);
+  useEffect(() => {
+    if (!(me && me.id)) {
+      Router.push("/");
+    }
+  }, [me && me.id]);
+
+  if (!me) {
+    alert("로그인을 하지 않았습니다.");
+    return null;
+  }
+
   return (
     <>
       <Head>
