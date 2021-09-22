cf298cc4e8952f10bf70f2a41cc16454af7a8d79
jungjuyoung <nwwn1004@naver.com>
2021-09-23 03:12:00 +0900

    feat:implement infinite scrolling

--- prepare/front/pages/index.js
+++ prepare/front/pages/index.js
@@ -10,32 +10,36 @@ import { LOAD_POSTS_REQUEST } from "../reducers/post";
 const Home = () => {
   const dispatch = useDispatch();
   const { me } = useSelector(state => state.user);
-  const { mainPosts } = useSelector(state => state.post);
-
+  const { mainPosts, hasMorePosts, loadPostsLoading } = useSelector(
+    state => state.post
+  );
+  console.log(mainPosts);
   useEffect(() => {
     dispatch({
       type: LOAD_POSTS_REQUEST,
     });
   }, []);
-  // useEffect(() => {
-  //   function onScroll() {
-  //     if (
-  //       window.scrollY + document.documentElement.clientHeight >
-  //       document.documentElement.scrollHeight - 300
-  //     ) {
-  //       if (hasMorePosts && !loadPostsLoading) {
-  //         dispatch({
-  //           type: LOAD_POSTS_REQUEST,
-  //           data: mainPosts[mainPosts.length - 1].id,
-  //         });
-  //       }
-  //     }
-  //   }
-  //   window.addEventListener("scroll", onScroll);
-  //   return () => {
-  //     window.removeEventListener("scroll", onScroll);
-  //   };
-  // }, [mainPosts, hasMorePosts, loadPostsLoading]);
+
+  useEffect(() => {
+    function onScroll() {
+      if (
+        window.scrollY + document.documentElement.clientHeight >
+        document.documentElement.scrollHeight - 300
+      ) {
+        if (hasMorePosts && !loadPostsLoading) {
+          dispatch({
+            type: LOAD_POSTS_REQUEST,
+            data: mainPosts[mainPosts.length - 1].id,
+          });
+        }
+      }
+    }
+    window.addEventListener("scroll", onScroll);
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, [mainPosts, hasMorePosts, loadPostsLoading]);
+
   return (
     <AppLayout>
       {me && <PostForm />}
