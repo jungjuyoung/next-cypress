ee726ad84319fd3758677d694aa24f09245f51ee
jungjuyoung <nwwn1004@naver.com>
2021-10-01 13:16:57 +0900

    style: code formatting

--- prepare/front/store/configureStore.js
+++ prepare/front/store/configureStore.js
@@ -13,6 +13,7 @@ const configureStore = context => {
     process.env.NODE_ENV === "production"
       ? compose(applyMiddleware(...middlewares))
       : composeWithDevTools(applyMiddleware(...middlewares));
+
   const store = createStore(reducer, enhancer);
   store.sagaTask = sagaMiddleware.run(rootSaga);
   return store;
