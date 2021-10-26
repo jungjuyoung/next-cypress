ee9fbd6ab0bd317003d4fea642cc1eff5fa5f6e4
jungjuyoung <nwwn1004@naver.com>
2021-10-26 21:55:19 +0900

    fix: edit profile page and installed moment and swr library

--- prepare/front/pages/profile.js
+++ prepare/front/pages/profile.js
@@ -1,55 +1,93 @@
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React, { useCallback, useEffect, useState } from "react";
 import Head from "next/head";
+import { useSelector } from "react-redux";
+import Router from "next/router";
+import { END } from "redux-saga";
+import axios from "axios";
+import useSWR from "swr";
 
 import AppLayout from "../components/AppLayout";
-import NickNameEditForm from "../components/nicknameEditForm";
+import NicknameEditForm from "../components/NicknameEditForm";
 import FollowList from "../components/FollowList";
-import Router from "next/router";
-import {
-  LOAD_FOLLOWERS_REQUEST,
-  LOAD_FOLLOWINGS_REQUEST,
-} from "../reducers/user";
+import { LOAD_MY_INFO_REQUEST } from "../reducers/user";
+import wrapper from "../store/configureStore";
+
+const fetcher = url =>
+  axios.get(url, { withCredentials: true }).then(result => result.data);
 
 const Profile = () => {
-  const dispatch = useDispatch();
+  const [followingsLimit, setFollowingsLimit] = useState(3);
+  const [followersLimit, setFollowersLimit] = useState(3);
+  const { data: followingsData, error: followingError } = useSWR(
+    `http://localhost:3065/user/followings?limit=${followingsLimit}`,
+    fetcher
+  );
+  const { data: followersData, error: followerError } = useSWR(
+    `http://localhost:3065/user/followers?limit=${followersLimit}`,
+    fetcher
+  );
   const { me } = useSelector(state => state.user);
-  console.log(`me: ${JSON.stringify(me)}`);
 
   useEffect(() => {
-    dispatch({
-      type: LOAD_FOLLOWERS_REQUEST,
-    });
-    dispatch({
-      type: LOAD_FOLLOWINGS_REQUEST,
-    });
-  }, []);
-
-  useEffect(() => {
-    if (!me?.id) {
+    if (!(me && me.id)) {
       Router.push("/");
     }
-  }, [me?.id]);
+  }, [me && me.id]);
 
-  if (!me) {
-    alert("로그인을 하지 않았습니다.");
-    return null;
+  const loadMoreFollowers = useCallback(() => {
+    setFollowersLimit(prev => prev + 3);
+  }, []);
+
+  const loadMoreFollowings = useCallback(() => {
+    setFollowingsLimit(prev => prev + 3);
+  }, []);
+
+  if (followerError || followingError) {
+    console.error(followerError || followingError);
+    return "팔로잉/팔로워 로딩 중 에러가 발생했습니다.";
   }
 
+  if (!me) {
+    return "내 정보 로딩중...";
+  }
   return (
     <>
       <Head>
-        <meta charSet="utf-8" />
-        <title>내프로필 | nodebird</title>
+        <title>내 프로필 | NodeBird</title>
       </Head>
-
       <AppLayout>
-        <NickNameEditForm />
-        <FollowList header="팔로워 리스트" data={me.Followers} />
-        <FollowList header="팔로잉 리스트" data={me.Followings} />
+        <NicknameEditForm />
+        <FollowList
+          header="팔로잉"
+          data={followingsData}
+          onClickMore={loadMoreFollowings}
+          loading={!followingError && !followingsData}
+        />
+        <FollowList
+          header="팔로워"
+          data={followersData}
+          onClickMore={loadMoreFollowers}
+          loading={!followerError && !followersData}
+        />
       </AppLayout>
     </>
   );
 };
+
+export const getServerSideProps = wrapper.getServerSideProps(async context => {
+  console.log("getServerSideProps start");
+  console.log(context.req.headers);
+  const cookie = context.req ? context.req.headers.cookie : "";
+  axios.defaults.headers.Cookie = "";
+  if (context.req && cookie) {
+    axios.defaults.headers.Cookie = cookie;
+  }
+  context.store.dispatch({
+    type: LOAD_MY_INFO_REQUEST,
+  });
+  context.store.dispatch(END);
+  console.log("getServerSideProps end");
+  await context.store.sagaTask.toPromise();
+});
 
 export default Profile;
